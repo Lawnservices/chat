32a9c25c6eab46f4e19c2812bea3982468cd6e69
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-01-21 17:57:48 -0500

    fix

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ io.on('connection', socket => {
     });
 });
 
-http.listen(3000, () => {
-    console.log('Servidor funcionando en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
